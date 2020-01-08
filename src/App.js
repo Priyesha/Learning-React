3c55dffffc65ec61ca347aca6c37923e455ce59e
Priyesha <priyeshayadav9192@gmail.com>
2020-01-08 23:08:22 +0530

    Functional component with named export

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import { Message } from './components/Message';
 
 function App() {
   return (
     <div className="App">
-    <Greet></Greet>
+    <Greet/>
+    <Message/>
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+// using arrow function and named export
+export const Message = () => <h3>A message for you!!</h3>
