9a013878aeb7c1d1d84709bf48c54a2500ee6fa0
Priyesha <priyeshayadav9192@gmail.com>
2020-01-20 23:08:22 +0530

    added callback function for setState method

--- src/components/stateComponent.js
+++ src/components/stateComponent.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counterValue: 1,
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            counterValue: 1
-        }
-    }
+  changeState() {
+    // setState method is asynchronous
+    this.setState(
+      {
+        counterValue: this.state.counterValue + 1,
+      },
+      // second parameter is callback method
+      () =>
+        console.log("value from callback method is", this.state.counterValue)
+    );
 
-    changeState() {
-        this.setState({
-            counterValue: this.state.counterValue + 1
-        })
-    }
+    // will give previous value of counter since setState is asynchronous
+    console.log("value after setState", this.state.counterValue);
+  }
 
-    render() {
+  render() {
     return (
-    <div>
-    <h1>My counter is {this.state.counterValue}</h1>
-    <button onClick={ () => this.changeState() }>Increment</button>
-    </div>
-    )}
-} 
+      <div>
+        <h1>My counter is {this.state.counterValue}</h1>
+        <button onClick={() => this.changeState()}>Increment</button>
+      </div>
+    );
+  }
+}
 
-export default Counter
+export default Counter;
