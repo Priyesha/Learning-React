35b2f99720c46045940e642fe11b544a0ce26b10
Priyesha <priyeshayadav9192@gmail.com>
2020-01-20 23:08:22 +0530

    passing function as arguement to setState to calculate value based on prevState

--- src/components/stateComponent.js
+++ src/components/stateComponent.js
@@ -10,24 +10,33 @@ class Counter extends Component {
 
   changeState() {
     // setState method is asynchronous
-    this.setState(
-      {
-        counterValue: this.state.counterValue + 1,
-      },
-      // second parameter is callback method
-      () =>
-        console.log("value from callback method is", this.state.counterValue)
-    );
+
+    // this.setState(
+    //   {
+    //     counterValue: this.state.counterValue + 1,
+    //   },
+    //   () =>
+    //     console.log("value from callback method is", this.state.counterValue)
+    // );
 
     // will give previous value of counter since setState is asynchronous
-    console.log("value after setState", this.state.counterValue);
+    // console.log("value after setState", this.state.counterValue);
+
+    this.setState(prevState => ({
+      counterValue: prevState.counterValue + 1
+    }));
+  }
+
+  incrementTwice() {
+      this.changeState();
+      this.changeState();
   }
 
   render() {
     return (
       <div>
         <h1>My counter is {this.state.counterValue}</h1>
-        <button onClick={() => this.changeState()}>Increment</button>
+        <button onClick={() => this.incrementTwice()}>Increment</button>
       </div>
     );
   }
